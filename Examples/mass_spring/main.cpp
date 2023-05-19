2626e615d583812a9bcca98a7f8006ef4b657497
pressureless <190361902@qq.com>
2023-05-19 17:22:20 -0400

    Mass-spring with bunny

--- Examples/mass_spring/main.cpp
+++ Examples/mass_spring/main.cpp
@@ -32,6 +32,7 @@ bool running = false;
 double mass = 1.0;
 double stiffness = 5e5;
 double damping = 5;
+// double damping = 15;
 double dt = 2e-4;
 double eps = 1e-6;
 
@@ -39,50 +40,58 @@ std::vector<Eigen::Matrix<double, 3, 1>> OriginalPosition;
 std::vector<Eigen::Matrix<double, 3, 1>> Position;
 std::vector<Eigen::Matrix<double, 3, 1>> Velocity;
 std::vector<Eigen::Matrix<double, 3, 1>> Force;
- 
+heartlib *_heartlib;
 void update(){
-    heartlib ihla(tet_mesh, OriginalPosition, mass, damping, stiffness, dt, bottom_z);
-    for (int i = 0; i < meshV.rows(); ++i)
+    int TIMES = 25;
+    for (int i = 0; i < TIMES; ++i)
     {
-        Velocity[i] = Eigen::Matrix<double, 3, 1>::Zero();
-        Force[i] = Eigen::Matrix<double, 3, 1>::Zero();
-    } 
-    // while(true){
-        int TIMES = 25;
-        for (int i = 0; i < TIMES; ++i)
-        {
-            #pragma omp parallel for schedule(static) num_threads(omp_get_thread_num())
-            for (int i = 0; i < meshV.rows(); ++i)
-            {
-                std::tuple< Eigen::Matrix<double, 3, 1>, Eigen::Matrix<double, 3, 1> > tuple = ihla.ApplyForces(i, Velocity, Force, Position);
-                Velocity[i] = std::get<0>(tuple);
-                Position[i] = std::get<1>(tuple);
-            } 
-            
-            #pragma omp parallel for schedule(static) num_threads(omp_get_thread_num())
-            for (int i = 0; i < meshV.rows(); ++i)
-            {
-                std::tuple< Eigen::Matrix<double, 3, 1>, Eigen::Matrix<double, 3, 1> > tuple = ihla.ComputeInternalForces(i, Velocity, Position);
-                Velocity[i] = std::get<0>(tuple);
-                Force[i] = std::get<1>(tuple);
-            } 
-        } 
-        double min_diff = 1000;
-        double max_diff = 0;
+        #pragma omp parallel for schedule(static) num_threads(omp_get_thread_num())
         for (int i = 0; i < meshV.rows(); ++i)
         {
-            double norm = (Position[i]-OriginalPosition[i]).norm();
-            if (norm < min_diff)
-            {
-                min_diff = norm;
-            }
-            if(norm > max_diff){
-                max_diff = norm;
-            } 
+            std::tuple< Eigen::Matrix<double, 3, 1>, Eigen::Matrix<double, 3, 1> > tuple = _heartlib->ApplyForces(i, Velocity, Force, Position);
+            Velocity[i] = std::get<0>(tuple);
+            Position[i] = std::get<1>(tuple);
         } 
-        std::cout<<"After updating, min offset: "<<min_diff<<", max offset: "<<max_diff<<std::endl;
-        polyscope::getSurfaceMesh("my mesh")->updateVertexPositions(Position);
-    // }
+        
+        #pragma omp parallel for schedule(static) num_threads(omp_get_thread_num())
+        for (int i = 0; i < meshV.rows(); ++i)
+        {
+            std::tuple< Eigen::Matrix<double, 3, 1>, Eigen::Matrix<double, 3, 1> > tuple = _heartlib->ComputeInternalForces(i, Velocity, Position);
+            Velocity[i] = std::get<0>(tuple);
+            Force[i] = std::get<1>(tuple);
+        } 
+    } 
+    // double min_diff = 1000;
+    // double max_diff = 0;
+    // for (int i = 0; i < meshV.rows(); ++i)
+    // {
+    //     double norm = (Position[i]-OriginalPosition[i]).norm();
+    //     if (norm < min_diff)
+    //     {
+    //         min_diff = norm;
+    //     }
+    //     if(norm > max_diff){
+    //         max_diff = norm;
+    //     } 
+    // } 
+    // std::cout<<"After updating, min offset: "<<min_diff<<", max offset: "<<max_diff<<std::endl;
+    polyscope::getSurfaceMesh("my mesh")->updateVertexPositions(Position);
+}
+
+void reset(){
+    Eigen::Matrix<double, 3, 1> initV;
+    initV<< -10, 0, 0;
+    // initV<< -15, 0, 0;
+    Eigen::Matrix<double, 3, 1> initF;
+    initF<< -1000, -50000, 0;
+    // initF<< -14000, -100000, 0;
+    #pragma omp parallel for schedule(static) num_threads(omp_get_thread_num())
+    for (int i = 0; i < meshV.rows(); ++i)
+    {
+        Position[i] = OriginalPosition[i]; 
+        Velocity[i] = initV.transpose(); 
+        Force[i] = initF.transpose(); 
+    }  
 }
 
 
@@ -90,6 +99,9 @@ void myCallback()
 { 
     if (ImGui::Button("Start/stop simulation")){ 
         running = !running;
+    } 
+    if (ImGui::Button("Reset")){ 
+        reset();
     } 
     if (running)
     {
@@ -106,17 +118,18 @@ int main(int argc, const char * argv[]) {
     // igl::readOBJ(argc>1?argv[1]:DATA_PATH / "camelhead-decimate-qslim.obj", meshV, meshF); 
     // igl::readOFF(argc>1?argv[1]:DATA_PATH /"bunny_200.off", meshV, meshF);
     igl::readMESH(argc>1?argv[1]:DATA_PATH / "bunny_200.mesh", meshV, T, meshF);   
-    double angle = 0.4;
-    Eigen::Matrix<double, 3, 3> rotation;
-    rotation << std::cos(angle), -std::sin(angle), 0,
-                std::sin(angle), std::cos(angle), 0,
-                0, 0, 1;
-    Eigen::Matrix<double, 3, 1> translation;
-    translation << 0, 0, 0; 
-    for (int i = 0; i < meshV.rows(); ++i)
-    {
-        meshV.row(i) = rotation * meshV.row(i).transpose() + translation;
-    } 
+    // igl::readMESH(argc>1?argv[1]:DATA_PATH / "bunny1k.mesh", meshV, T, meshF);   
+    // double angle = 0.4;
+    // Eigen::Matrix<double, 3, 3> rotation;
+    // rotation << std::cos(angle), -std::sin(angle), 0,
+    //             std::sin(angle), std::cos(angle), 0,
+    //             0, 0, 1;
+    // Eigen::Matrix<double, 3, 1> translation;
+    // translation << 0, 0, 0; 
+    // for (int i = 0; i < meshV.rows(); ++i)
+    // {
+    //     meshV.row(i) = rotation * meshV.row(i).transpose() + translation;
+    // } 
 
     // the lowest point is 0.3
     double minY = 1000;
@@ -128,7 +141,7 @@ int main(int argc, const char * argv[]) {
             minY = meshV(i, 1);
         }
     } 
-    offset = 0.03 - minY;
+    offset = 0.3+0.03 - minY;
     for (int i = 0; i < meshV.rows(); ++i)
     {
         meshV(i, 1) += offset;
@@ -137,16 +150,18 @@ int main(int argc, const char * argv[]) {
     tet_mesh.initialize(T); 
     for (int i = 0; i < meshV.rows(); ++i)
     {
-        Position.push_back(meshV.row(i).transpose());
         OriginalPosition.push_back(meshV.row(i).transpose());
-        Velocity.push_back(Eigen::Matrix<double, 3, 1>::Zero());
-        Force.push_back(Eigen::Matrix<double, 3, 1>::Zero());
     } 
+    _heartlib = new heartlib(tet_mesh, OriginalPosition, mass, damping, stiffness, dt, bottom_z);
+    Position.resize(OriginalPosition.size());
+    Velocity.resize(OriginalPosition.size());
+    Force.resize(OriginalPosition.size());
+    reset();
     // Initialize polyscope 
     polyscope::init();   
     polyscope::registerSurfaceMesh("my mesh", OriginalPosition, meshF);
     // polyscope::options::groundPlaneMode = polyscope::GroundPlaneMode::ShadowOnly;
-    // polyscope::options::groundPlaneHeightFactor = -polyscope::state::lengthScale * 1e-4 + std::get<0>(polyscope::state::boundingBox)[1]; // adjust the plane height
+    polyscope::options::groundPlaneHeightFactor = 1; // adjust the plane height
     // polyscope::state::lengthScale = 0;
     polyscope::state::userCallback = myCallback;
     polyscope::show();
